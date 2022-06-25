1997089c702bd0aa44f5b884c43c2f6f5ad9f454
"gymcode" <"kyleabs20@gmail.com">
2022-06-25 23:31:56 +0000

    Initial commit for the trial project

--- contracts/4_Test.sol
+++ contracts/4_Test.sol
@@ -1,10 +1,26 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Test {
-    bool constant trueOrFalse = true
-    uint number = 200000000;
-    int negativeNumber = -2
-    string randomString = "Hello"
+    // bool constant trueOrFalse = true
+    // uint number = 200000000;
+    // int negativeNumber = -2
+    // string randomString = "Hello"
     // initializing an array of strings
-    string[] public firstArrString = ["hi", "hello"]
+    // string[] public firstArrString = ["hi", "hello"];
+
+    Person public person;
+
+    struct Person{
+        string firstname;
+        string lastname;
+        uint256 age;
+    }
+
+    function doThings() public {
+        person = Person({
+            firstname: "Kenneth",
+            lastname: "Lartey",
+            age: 22
+        });
+    }
 }
