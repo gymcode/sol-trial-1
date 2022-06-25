9a6cafb3880367fb57b233caad97f072c7a07cbd
Remix Workspace <>
2022-06-25 23:15:02 +0000

    Initial commit for the trial project

---
+++ contracts/4_Test.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Test {
+    bool constant trueOrFalse = true
+    uint number = 200000000;
+    int negativeNumber = -2
+    string randomString = "Hello"
+    // initializing an array of strings
+    string[] public firstArrString = ["hi", "hello"]
+}
