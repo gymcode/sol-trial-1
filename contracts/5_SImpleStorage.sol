3b51b3c4863b600bb8ba90fe51ea2e357efb1ef6
"gymcode" <"kyleabs20@gmail.com">
2022-07-02 22:55:52 +0000

    Progress

---
+++ contracts/5_SImpleStorage.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8;
+
+contract SimpleStorage {
+    
+    uint256 public favoriteNumber;
+
+    // function 
+    function store(uint256 numberToStore) public{
+        favoriteNumber = numberToStore;
+    }
+}
