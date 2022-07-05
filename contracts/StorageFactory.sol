431e064098010bed7b0fae5cb26608466f7a293e
"gymcode" <"kyleabs20@gmail.com">
2022-07-05 00:21:07 +0000

    still on imports

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -2,10 +2,25 @@
 pragma solidity ^0.8.8;
 import "./SimpleStorage.sol";
 
+// manager of all of our simple storage stuff
 contract StorageFactory{
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public simpleStorageArray;
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        // assigning the simple storage to a variable
+        SimpleStorage simpleStorage = new SimpleStorage();
+        /// pushing it into an array
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public{
+        // get the address using the simpleStorage index
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retrieve();
     }
 }
